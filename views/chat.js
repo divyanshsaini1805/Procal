a6f2f35d4e0899a404ea18e24bbdc09e3b7dab4a
Divyansh Saini <divyanshsaini1805@gmail.com>
2020-11-24 13:54:44 +0530

    Chatbox updated

--- views/chat.js
+++ views/chat.js
@@ -1,13 +1,31 @@
-let chat=document.querySelector(".chats");
+// let chat =  document.querySelector(".chats");
 
 
-chat.addEventListener("click",()=>{
-     chat.style.display="block";
+// chat.addEventListener("click",()=>{
+//      chat.style.display="block";
 
-    chat.classList.add("size");
+//     chat.classList.add("size");
 
-         setTimeout(function(){
-            chat.classList.toggle("size");
-         },5000); 
-});  
+//          setTimeout(function(){
+//             chat.classList.toggle("size");
+//          },5000); 
+// });  
   
+let chat =  Array.from(document.getElementsByClassName('chats'));
+
+chat.forEach(chatBox)
+  
+function chatBox(element){
+   
+
+   element.addEventListener("click",()=>{
+      element.style.display="block";
+ 
+     element.classList.add("size");
+ 
+          setTimeout(function(){
+             element.classList.toggle("size");
+          },5000); 
+ });  
+
+}
